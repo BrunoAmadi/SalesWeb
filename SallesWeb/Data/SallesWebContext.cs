6b676f734736ffb76dee4e457cdea45afdcf4b63
Bruno Augusto <Brunoaug95@gmail.com>
2023-10-26 13:13:04 -0300

    Other entities and second migration

--- SallesWeb/Data/SallesWebContext.cs
+++ SallesWeb/Data/SallesWebContext.cs
@@ -14,6 +14,8 @@ namespace SallesWeb.Data
         {
         }
 
-        public DbSet<SallesWeb.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- SallesWeb/Models/Department.cs
+++ SallesWeb/Models/Department.cs
@@ -10,6 +10,7 @@ namespace SallesWeb.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
 
         public Department()
@@ -23,5 +24,17 @@ namespace SallesWeb.Models
             Id = id;
             Name = name;
         }
+
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+
     }
 }
